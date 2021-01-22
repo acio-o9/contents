b7075d9227d7e1fbb2fb4fb1eac161439ea32c32
acio-o9 <aco6280113@gmail.com>
2021-01-23 08:39:46 +0900

    [feat] implement material-ui components
    - Header imports AppBar
    - Layout imports Container

--- src/application/components/Header.js
+++ src/application/components/Header.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { AppBar } from '@material-ui/core';
 
 class Header extends Component {
   render() {
     return (
       <div>
-        <h1>This is Header</h1>
+        <AppBar position="static">
+          <p>This is Header.</p>
+        </AppBar>
       </div>
     )
   }
--- src/application/components/Layout.js
+++ src/application/components/Layout.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import Container from '@material-ui/core/Container';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 class Layout extends Component {
   render() {
     return (
-      <div>
+      <>
         <Header />
-        <section>
-          {this.props.children}
-        </section>
+        <Container maxWidth="sm">
+          <div>
+            {this.props.children}
+          </div>
+        </Container>
         <Footer />
-      </div>
+      </>
     )
   }
 }
