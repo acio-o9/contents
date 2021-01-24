27316eb9ca78bbed0aabd5814b5c8c8c618ce9fd
acio-o9 <aco6280113@gmail.com>
2021-01-24 12:49:41 +0900

    [feat] modify environment behavior

--- src/application/pages/index.js
+++ src/application/pages/index.js
@@ -11,28 +11,42 @@ function Index( {articles} ) {
 
 export async function getStaticProps() {
   // TODO fetching resource using an external module
-  const mock = {
-    contents: [
-      {
-        id: 'w8ywix2yy',
-        publishedAt: '2021-01-23T05:26:58.067Z',
-        title: 'tetetest',
-        body: '<p>ほんぶんです</p>'
+  if (process.env.NODE_ENV == 'development') {
+    const mock = {
+      contents: [
+        {
+          id: 'w8ywix2yy',
+          publishedAt: '2021-01-23T05:26:58.067Z',
+          title: 'tetetest',
+          body: '<p>ほんぶんです</p>'
+        },
+        {
+          id: '5sa6f6jsc',
+          publishedAt: '2020-10-27T14:46:35.326Z',
+          title: 'テスト',
+          body: '<p>記事テスト</p>'
+        }
+      ],
+      totalCount: 2,
+      offset: 0,
+      limit: 10
+    };
+    return {
+      props: {
+        articles: mock.contents,
       },
-      {
-        id: '5sa6f6jsc',
-        publishedAt: '2020-10-27T14:46:35.326Z',
-        title: 'テスト',
-        body: '<p>記事テスト</p>'
-      }
-    ],
-    totalCount: 2,
-    offset: 0,
-    limit: 10
-  };
+    }
+  }
+
+  const key = {
+      headers: {'X-API-KEY': process.env.CMS_API_KEY}
+  }
+  const endpoint = process.env.CMS_ENDPOINT;
+  const res = await fetch(endpoint + 'articles', key)
+  const json = await res.json();
   return {
     props: {
-      articles: mock.contents,
+      articles: json.contents,
     },
   }
 }
