5a058819608c51e58cd7feef6fbedb334cd3ac43
acio-o9 <aco6280113@gmail.com>
2021-01-24 23:18:01 +0900

    [add] set a Copyright

--- src/application/components/Footer.js
+++ src/application/components/Footer.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { AppBar } from '@material-ui/core';
 
 class Footer extends Component {
   render() {
     return (
       <div>
-        <p>This is Footer</p>
+        <AppBar position="static">
+          <p style={{padding:10 + 'px', fontSize: 9 + 'px'}}>
+            Copyright (C)2021-2021 maru34.com All Rights Reserved.
+          </p>
+        </AppBar>
       </div>
     )
   }
