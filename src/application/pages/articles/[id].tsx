e1f176e6ebc63e33533a43cd5dc9b22e105231ae
acio-o9 <aco6280113@gmail.com>
2021-01-28 08:53:47 +0900

    [feat] generage detail pages

---
+++ src/application/pages/articles/[id].tsx
@@ -0,0 +1,47 @@
+import {GetStaticPaths} from 'next';
+import { FC } from 'react';
+import Layout from '../../components/Layout';
+import ClientInterface from '../../modules/api/ClinetInterface';
+import MicroCmsClient from '../../modules/api/MicroCmsClient';
+import MockClient from '../../modules/api/MockClient';
+
+const Index: FC<{ article: any }> = ({article}) => {
+  return (
+    <Layout title={"maru34 - WEB系エンジニアの趣味ブログ"}>
+      <h1>{article.title}</h1>
+      <div>
+        <span>Date: {article.publishedAt}</span>
+        <span>{article.body}</span>
+      </div>
+    </Layout>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  let client: ClientInterface = (process.env.NODE_ENV == 'development')
+    ? new MockClient('articles')
+    : new MicroCmsClient('articles');
+
+  const result = await client.findAll();
+  const paths = result.contents.map((content: any) => `/articles/${content.id}`)
+
+  return { paths, fallback: false}
+}
+
+export async function getStaticProps(content: any) {
+  const id = content.params.id
+  let client: ClientInterface = (process.env.NODE_ENV == 'development')
+    ? new MockClient('articles')
+    : new MicroCmsClient('articles');
+
+  const result = await client.find(id);
+
+  return {
+    props: {
+      article: result
+    },
+  }
+}
+
+export default Index
+
