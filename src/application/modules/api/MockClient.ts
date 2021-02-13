aa6849100a49d455333a82debe677b5f6c176295
acio-o9 <aco6280113@gmail.com>
2021-02-13 22:27:21 +0900

    [modify] update mock response

--- src/application/modules/api/MockClient.ts
+++ src/application/modules/api/MockClient.ts
@@ -14,7 +14,7 @@ class ArticleMockClient implements ClientInterface {
         id: 'w8ywix2yy',
         publishedAt: '2021-01-23T05:26:58.067Z',
         title: 'hogehoge',
-        body: '<p>ほんぶんです</p>'
+        body: "<h1 id=\"h8189c32b27\">構成だったり、苦労したことだったり、をつらつら書きます。</h1><p><br></p><ul><li>ほげ</li><li>ふが</li></ul><blockquote>ここは参照です</blockquote><p><u>あんだーらいん</u>を引いています<br>ここは<strong>重要</strong>です<br>こんな<em>引用</em>もしていたりします<br><s>訂正</s>します<br></p><h1 id=\"h4f8e1ebce5\">題目１</h1><h2 id=\"hb30eab9e7a\">さぶだいとる</h2><h3 id=\"h8d926f060a\">h3くらい</h3><p><br></p><pre><code>&lt;?php\n$list = [];\nforeach ($list as $content) {\n    echo $content;\n}</code></pre><p><br></p>"
       };
     } else {
       result = {
